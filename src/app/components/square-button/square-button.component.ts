695871042a9b0169168a9da210437470299e12b6
Catalina <catalinacalin777@gmail.com>
2022-10-30 02:16:43 +0300

    faq desktop

--- src/app/components/square-button/square-button.component.ts
+++ src/app/components/square-button/square-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'square-button',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./square-button.component.scss']
 })
 export class SquareButtonComponent implements OnInit {
+  @Input() color = '';
+  @Input() text = '';
   constructor() {}
 
   ngOnInit(): void {}
