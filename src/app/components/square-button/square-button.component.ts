bb521086dffec1904728504421ec50f4b57ea458
Catalina <catalinacalin777@gmail.com>
2022-11-01 22:09:28 +0200

    prices cards mobile

--- src/app/components/square-button/square-button.component.ts
+++ src/app/components/square-button/square-button.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SquareButtonComponent implements OnInit {
   @Input() color = '';
   @Input() text = '';
+  @Input() bordercolor = '';
+
   constructor() {}
 
   ngOnInit(): void {}
