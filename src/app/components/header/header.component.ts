7e6c0992c8764deb8d63d20dbfed0d5b1cc6287b
Lupu Cristian <lupu.cristian.dev@gmail.com>
2022-11-08 20:13:29 +0200

    update burger menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
   isOpenBurger: boolean = false;
+  isOpenSubmenu: boolean = false;
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
